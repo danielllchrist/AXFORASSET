e8c0a0af81c01f75ce631f21e8e3fd01e0684418
jesslyntanu <129813528+jesslyntanu@users.noreply.github.com>
2024-08-02 14:19:11 +0700

    Dialog Message Done

--- app/src/main/java/com/uxlab/axforasset/MainActivity.java
+++ app/src/main/java/com/uxlab/axforasset/MainActivity.java
@@ -1,5 +1,6 @@
 package com.uxlab.axforasset;
 
+import android.content.Intent;
 import android.os.Bundle;
 //import android.view.View;
 import android.widget.ArrayAdapter;
@@ -19,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        Intent intent = new Intent(MainActivity.this, GameAssetDetail.class);
+        startActivity(intent);
     }
 }
