571a8164719774c42c4055a8f4459ba3ef42ed27
Joxnhby <114238028+Joxnhby@users.noreply.github.com>
2024-08-09 09:33:33 +0700

    home kelar

---
+++ app/src/main/java/com/uxlab/axforasset/VPAdapter.java
@@ -0,0 +1,40 @@
+package com.uxlab.axforasset;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class VPAdapter extends FragmentPagerAdapter {
+    final ArrayList<Fragment> fragmentArrayList = new ArrayList<>();
+    final ArrayList<String> fragmentTitles = new ArrayList<>();
+
+    public VPAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentArrayList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentArrayList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragmentArrayList.add(fragment);
+        fragmentTitles.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitles.get(position);
+    }
+}
